6a87f3a90a285ff7e5734ccc566ee358371edddd
Thraka <thraka.junk@outlook.com>
2026-01-20 18:48:38 -0800

    Fix gradle tmp folder

--- gradlew.bat
+++ gradlew.bat
@@ -35,6 +35,11 @@ set APP_HOME=%DIRNAME%
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
+@rem Set temp dir to project build folder to avoid Windows cross-drive path issues
+set TEMP=%APP_HOME%build\tmp
+set TMP=%APP_HOME%build\tmp
+if not exist "%TEMP%" mkdir "%TEMP%"
+
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
